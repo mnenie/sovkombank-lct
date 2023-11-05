579f4e5186827b2efbbc0e3282d91999c741bcfc
mnenie <121057011+mneniee@users.noreply.github.com>
2023-11-05 13:22:30 +0300

    home

--- client/src/components/HomeTasks/HomeTasks.jsx
+++ client/src/components/HomeTasks/HomeTasks.jsx
@@ -1,10 +1,61 @@
-import React from 'react';
-import styles from './HomeTasks.module.scss'
-
+import React from "react";
+import styles from "./HomeTasks.module.scss";
 const HomeTasks = () => {
+  const itemsMap = [
+    {
+      id: 1,
+      title: "ВТБ на Ленинградке",
+      about: "Доставка карт и материалов",
+    },
+    {
+      id: 2,
+      title: "ВТБ на Краснодарке",
+      about: "Доставка Молчей",
+    },
+    {
+      id: 3,
+      title: "ВТБ на Московке",
+      about: "Епт",
+    },
+    {
+      id: 4,
+      title: "ВТБ на Хабаровске",
+      about: "Егор Суханов шо ты",
+    },
+  ];
+  const tasks = [
+    {
+      id: 1,
+      title: 'Флаеры'
+    },
+    {
+      id: 2,
+      title: 'Молч'
+    },
+    {
+      id: 3,
+      title: 'Дебетовая карта'
+    },
+  ]
   return (
     <div className={styles.tasks}>
-      
+      {itemsMap.map((item) => (
+        <div key={item.id} className={styles.task}>
+          <div className={styles.text}>
+            <h3>{item.title}</h3>
+            <p>{item.about}</p>
+          </div>
+          {tasks.map((task) => (
+            <div key={task.id} className={styles.sub_task}>
+              <span>{task.title}</span>
+              <div className={styles.route}>
+                <span>Построить маршрут</span>
+                <img src="/img/route.png" alt="" />
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,40 +1,17 @@
-// import { YMaps, Map, Placemark, RouteEditor,GeoObject } from '@pbe/react-yandex-maps';
-import styles from './home.module.scss'
-import HomeTasks from '@/components/HomeTasks/HomeTasks';
-
+import styles from "./home.module.scss";
+import HomeTasks from "@/components/HomeTasks/HomeTasks";
+import MapBlock from "@/components/Map/MapBlock";
 const Home = () => {
   return (
     <div className={styles.home}>
       <div className="container">
-        <HomeTasks />
+        <div className={styles.blocks}>
+          <div className={styles.block_1}>
+            <HomeTasks />
+          </div>
+          <MapBlock />
+        </div>
       </div>
-      {/* <YMaps>
-      <Map
-        defaultState={{
-          center: [55.751574, 37.573856],
-          zoom: 9,
-          controls: [],
-        }}
-      >
-        <Placemark geometry={[55.751574, 37.573856]}/>
-        <Placemark geometry={[55.684758, 37.738521]}/>
-        <RouteEditor />
-        <GeoObject
-        geometry={{
-        type: "Point",
-        coordinates: [
-          [55.751574, 37.573856],
-          [55.684758, 37.738521],
-        ],
-      }}
-      options={{
-        geodesic: true,
-        strokeWidth: 5,
-        strokeColor: "#F008",
-      }}
-    />
-      </Map>  
-      </YMaps> */}
     </div>
   );
 };
