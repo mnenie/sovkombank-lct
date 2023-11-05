5d638c052e2fb2b5ee579937e3459a3fdfba226d
mnenie <121057011+mneniee@users.noreply.github.com>
2023-11-05 15:31:52 +0300

    home

--- client/src/components/HomeTasks/HomeTasks.jsx
+++ client/src/components/HomeTasks/HomeTasks.jsx
@@ -1,67 +1,48 @@
-import React from "react";
 import styles from "./HomeTasks.module.scss";
 import Checkbox from "../ui/Checkbox/Checkbox";
-const HomeTasks = () => {
-  const itemsMap = [
-    {
-      id: 1,
-      title: "ВТБ на Ленинградке",
-    },
-    {
-      id: 2,
-      title: "ВТБ на Краснодарке",
-    },
-    {
-      id: 3,
-      title: "ВТБ на Московке",
-    },
-    {
-      id: 4,
-      title: "ВТБ на Хабаровске",
-    },
-  ];
-  const tasks = [
-    {
-      id: 1,
-      title: "Флаеры",
-      about: "Доставка карт и материалов",
-    },
-    {
-      id: 2,
-      title: "Молч",
-      about: "Обучение агента",
-    },
-    {
-      id: 3,
-      title: "Дебетовая карта",
-      about: "Выезд на точку для стимулирования выдач",
-    },
-  ];
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { v4 as uuidv4 } from "uuid";
+
+const HomeTasks = ({ tasks, itemsMap, checked, setChecked }) => {
+  const changeTasks = (taskId) => {
+    setChecked((prev) => ({ ...prev, [taskId]: !prev[taskId] }));
+  };
+  const filteredTasks = tasks.filter((task) => !checked[task.id]);
+
   return (
     <div className={styles.tasks}>
-      {itemsMap.map((item) => (
-        <div key={item.id} className={styles.task}>
-          <div className={styles.text}>
-            <h3>{item.title}</h3>
-            <div className={styles.state}>
-              <div className={styles.checkbox_items}>
-                <Checkbox />
-                Выполнено
-              </div>
+      <TransitionGroup>
+        {itemsMap.map((item) => (
+          <div className={styles.task}>
+            <div className={styles.text}>
+              <h3>{item.title}</h3>
               <div className={styles.route}>
                 <span>Построить маршрут</span>
-                {/* <img src="/img/route.png" alt="" /> */}
               </div>
             </div>
+            {filteredTasks.map((task) => (
+              <CSSTransition
+                key={task.id}
+                timeout={500}
+                classNames={styles.sub_task}
+              >
+                <div className={styles.sub_task}>
+                  <span>{task.title}</span>
+                  <div className={styles.state}>
+                    <span>{task.about}</span>
+                    <div className={styles.checkbox_items}>
+                      <Checkbox
+                        checked={checked[task.id]}
+                        setChecked={() => changeTasks(task.id)}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </CSSTransition>
+            ))}
           </div>
-          {tasks.map((task) => (
-            <div key={task.id} className={styles.sub_task}>
-              <span>{task.title}</span>
-              <span>{task.about}</span>
-            </div>
-          ))}
-        </div>
-      ))}
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
--- client/src/components/ui/Checkbox/Checkbox.jsx
+++ client/src/components/ui/Checkbox/Checkbox.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import styles from './checkbox.module.scss'
-const Checkbox = () => {
-  const [checked, setChecked] = useState(true);
+const Checkbox = ({checked, setChecked}) => {
   return (
     <div className={styles.checkbox}>
       <input
