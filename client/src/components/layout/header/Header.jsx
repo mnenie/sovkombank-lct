1c3a88bdacb76df2092adba50779cea8f6bc600c
mnenie <121057011+mneniee@users.noreply.github.com>
2023-11-06 19:25:15 +0300

    fix: header

--- client/src/components/layout/header/Header.jsx
+++ client/src/components/layout/header/Header.jsx
@@ -1,52 +1,83 @@
-import React, {useState, useEffect} from "react";
+import { useState, useEffect, useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { HOME_ROUTE } from "@/utils/consts";
 import styles from "./header.module.scss";
 import HeaderMobile from "./HeaderMobile";
-const Header = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+import { observer } from "mobx-react-lite";
+import { Context } from "../../../main";
 
+const Header = observer(() => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [dropdown, setDropdown] = useState(false);
+  const { user } = useContext(Context);
   const handleResize = () => {
     setWindowWidth(window.innerWidth);
   };
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
-  }, []); 
+  }, []);
 
   const isMobile = windowWidth <= 650;
+  const isAuth = localStorage.getItem("isAuth");
 
+  let name = null;
+  if (localStorage.getItem("user")) {
+    name = JSON.parse(localStorage.getItem("user")).name;
+  }
+  const changeDropdown = () => {
+    setDropdown(!dropdown);
+  };
+  const changeAuth = async () => {
+    await user.logout();
+    location.reload();
+  };
   return (
     <header>
       <div className="container">
         {isMobile ? (
           <HeaderMobile />
         ) : (
-        <div className={styles.header_blocks}>
-          <div className={styles.logo_header}>
-            <NavLink className={styles.img_header} to={HOME_ROUTE}>
-              <img style={{ width: "9rem" }} src="/icons/logo.svg" alt="" />
-            </NavLink>
-            <div className={styles.text_header}>
-              <span>Сотрудникам банка</span>
-              <div className={styles.auth_header_line}></div>
-              <a href="tel: +78001000006">8 800 100 0006</a>
+          <div className={styles.header_blocks}>
+            <div className={styles.logo_header}>
+              <NavLink className={styles.img_header} to={HOME_ROUTE}>
+                <img style={{ width: "9rem" }} src="/icons/logo.svg" alt="" />
+              </NavLink>
+              <div className={styles.text_header}>
+                <span>Сотрудникам банка</span>
+                <div className={styles.auth_header_line}></div>
+                <a href="tel: +78001000006">8 800 100 0006</a>
+              </div>
+            </div>
+            <div className={styles.auth_header}>
+              {isAuth ? (
+                <div className={styles.auth_new}>
+                  <span>{name}</span>
+                  <img onClick={changeDropdown} src="/icons/arrow.svg" alt="" />
+                  {dropdown ? (
+                    <div onClick={changeAuth} className={styles.dropdown}>
+                      Выход
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
+              ) : (
+                <NavLink className={styles.link_header} to={HOME_ROUTE}>
+                  Войти
+                  <img src="/icons/entry.svg" alt="" />
+                </NavLink>
+              )}
             </div>
           </div>
-          <div className={styles.auth_header}>
-            <NavLink className={styles.link_header} to={HOME_ROUTE}>
-              Войти
-              <img src="/icons/entry.svg" alt="" />
-            </NavLink>
-          </div>
-        </div>
         )}
       </div>
     </header>
   );
-};
+});
 
 export default Header;
