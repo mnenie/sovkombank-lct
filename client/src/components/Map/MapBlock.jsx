036a622847b3849e469e0c35b45d747c56f5802a
mnenie <121057011+mneniee@users.noreply.github.com>
2023-11-07 12:18:44 +0300

    new: map

--- client/src/components/Map/MapBlock.jsx
+++ client/src/components/Map/MapBlock.jsx
@@ -1,42 +1,48 @@
-import styles from "./map.module.scss";
-import {
-  YMaps,
-  Map,
-  Placemark,
-  RouteEditor,
-  GeoObject,
-} from "@pbe/react-yandex-maps";
+import { load } from "@2gis/mapgl";
+import { useContext, useEffect } from "react";
+import MapWrapper from "./MapWrapper";
+import { MapContext } from "./MapProvider";
+import { Directions } from "@2gis/mapgl-directions";
+
 const MapBlock = () => {
+  const [_, setMapInstance] = useContext(MapContext);
+
+  useEffect(() => {
+    let map;
+
+    load().then((mapglAPI) => {
+      map = new mapglAPI.Map("map-container", {
+        center: [55.31878, 25.23584],
+        zoom: 13,
+        // key: "52",
+      });
+
+      const directions = new Directions(map, {
+        // directionsApiKey: '52',
+      });
+
+      directions.carRoute({
+        points: [
+          [55.27887, 25.21001],
+          [55.30771, 25.20314],
+        ],
+      });
+
+      setMapInstance(map);
+    });
+
+    // Удаляем карту при размонтировании компонента
+    return () => {
+      if (map) {
+        map.destroy();
+      }
+    };
+  }, [setMapInstance]);
+
   return (
-    <YMaps>
-      <div className={styles.map}>
-        <Map
-          defaultState={{
-            center: [55.751574, 37.573856],
-            zoom: 9,
-            controls: [],
-          }}
-        >
-          <Placemark geometry={[55.751574, 37.573856]} />
-          <Placemark geometry={[55.684758, 37.738521]} />
-          <RouteEditor />
-          <GeoObject
-            geometry={{
-              type: "Point",
-              coordinates: [
-                [55.751574, 37.573856],
-                [55.684758, 37.738521],
-              ],
-            }}
-            options={{
-              geodesic: true,
-              strokeWidth: 5,
-              strokeColor: "#F008",
-            }}
-          />
-        </Map>
-      </div>
-    </YMaps>
+    <div id="map-container" style={{ width: "100%", height: "100%" }}>
+      <MapWrapper />
+    </div>
   );
 };
 
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -2,23 +2,27 @@ import styles from "./home.module.scss";
 import HomeTasks from "@/components/HomeTasks/HomeTasks";
 import MapBlock from "@/components/Map/MapBlock";
 import { useEffect, useState } from "react";
-import {cloneDeep} from "lodash"
+import { cloneDeep } from "lodash";
 import TaskService from "../../services/TaskService";
 
 const Home = () => {
   const [tasks, setTasks] = useState([]);
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem("user"));
 
   useEffect(() => {
-    TaskService.getUserTasks(user.id).then((tasks) => {setTasks(tasks)  });
-  },[]);
+    TaskService.getUserTasks(user.id).then((tasks) => {
+      setTasks(tasks);
+    });
+  }, []);
 
   const deleteTask = (taskId, subtaskTitle) => {
-    const newTasks = cloneDeep(tasks); 
+    const newTasks = cloneDeep(tasks);
     for (let index = 0; index < newTasks.length; index++) {
       if (newTasks[index].task.id === taskId) {
-        newTasks[index].task.sub_task = newTasks[index].task.sub_task.filter(subtask => subtask.title !== subtaskTitle); 
-        console.log(newTasks[index])
+        newTasks[index].task.sub_task = newTasks[index].task.sub_task.filter(
+          (subtask) => subtask.title !== subtaskTitle
+        );
+        console.log(newTasks[index]);
         break;
       }
     }
@@ -31,7 +35,9 @@ const Home = () => {
           <div className={styles.block_1}>
             <HomeTasks tasks={tasks} deleteTask={deleteTask} />
           </div>
-          <MapBlock />
+          <div style={{ width: "100%", height: '400px' }}>
+            <MapBlock />
+          </div>
         </div>
       </div>
     </div>
