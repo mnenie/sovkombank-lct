f065b4634e0e8a78cd5255c277633d37ef99c9d1
mnenie <121057011+mneniee@users.noreply.github.com>
2023-11-08 11:33:22 +0300

    map

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,26 +1,22 @@
-import React, { useEffect } from 'react';
-import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import MapboxDirections from '@mapbox/mapbox-gl-directions';
-
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiYXlhYW56YXZlcmkiLCJhIjoiY2ttZHVwazJvMm95YzJvcXM3ZTdta21rZSJ9.WMpQsXd5ur2gP8kFjpBo8g';
-
-const App = () => {
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: this.mapWrapper,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-73.985664, 40.748514],
-      zoom: 12,
-    });
-    const directions = new MapboxDirections({
-      accessToken: mapboxgl.accessToken,
-      unit: 'metric',
-      profile: 'mapbox/driving',
-    });
-    map.addControl(directions, 'top-left');
-  });
-  return <div ref={(el) => (this.mapWrapper = el)} className="mapWrapper" />;
+import React, { useState } from "react";
+import ReactMapGl, {GeolocateControl, FullscreenControl, NavigationControl} from "react-map-gl";
+const Map = () => {
+  return (
+    <ReactMapGl
+      mapboxAccessToken="pk.eyJ1Ijoic3Rhc25vcm1hbiIsImEiOiJjbG9vNmhyengzNGN3MmtwbDlqYXA3ZnVpIn0.uHloBMG-PmI89AZi3ICtwA"
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14,
+      }}
+      style={{ width: 600, height: 400 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    >
+      <GeolocateControl />
+      <NavigationControl/>
+      <FullscreenControl />
+    </ReactMapGl>
+  );
 };
-export default App;
+
+export default Map;
